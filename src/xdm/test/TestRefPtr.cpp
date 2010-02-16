bde60f2eb231fd3fe0f7c3ba2951cd2f1f4025f5
shane <shane@50cd67bb-4cdf-4af9-8d16-de846d8cde40>
2010-02-16 15:01:52 +0000

    Added a test that dereferences a downcast RefPtr.

--- src/xdm/test/TestRefPtr.cpp
+++ src/xdm/test/TestRefPtr.cpp
@@ -1,24 +1,24 @@
 //==============================================================================
-// This software developed by Stellar Science Ltd Co and the U.S. Government.  
-// Copyright (C) 2009 Stellar Science. Government-purpose rights granted.      
-//                                                                             
-// This file is part of XDM                                                    
-//                                                                             
-// This program is free software: you can redistribute it and/or modify it     
-// under the terms of the GNU Lesser General Public License as published by    
-// the Free Software Foundation, either version 3 of the License, or (at your  
-// option) any later version.                                                  
-//                                                                             
-// This program is distributed in the hope that it will be useful, but WITHOUT 
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public        
-// License for more details.                                                   
-//                                                                             
-// You should have received a copy of the GNU Lesser General Public License    
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.       
-//                                                                             
+// This software developed by Stellar Science Ltd Co and the U.S. Government.
+// Copyright (C) 2009 Stellar Science. Government-purpose rights granted.
+//
+// This file is part of XDM
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 //------------------------------------------------------------------------------
-#define BOOST_TEST_MODULE RefPtr 
+#define BOOST_TEST_MODULE RefPtr
 #include <boost/test/unit_test.hpp>
 
 #include <xdm/ReferencedObject.hpp>
@@ -32,6 +32,23 @@ class AlsoDerived : public xdm::ReferencedObject {};
 void takesBaseClass( xdm::RefPtr< xdm::ReferencedObject > obj ) {
   BOOST_CHECK( true );
 }
+
+class TestBase : public xdm::ReferencedObject {
+public:
+  TestBase( int n ) : mInt( n ) {}
+  int getInt() const { return mInt; }
+private:
+  int mInt;
+};
+
+class TestDerived : public TestBase {
+public:
+  TestDerived( int n1, int n2 ) : TestBase( n1 ), mInt2( n2 ) {}
+  int getInt() const { return mInt2; } // Hides base class getInt
+  int getIntBase() const { return TestBase::getInt(); }
+private:
+  int mInt2;
+};
 
 BOOST_AUTO_TEST_CASE( templateAssignmentOperator ) {
   xdm::RefPtr< Derived > a( new Derived );
@@ -63,6 +80,16 @@ BOOST_AUTO_TEST_CASE( dynamicCast ) {
 BOOST_AUTO_TEST_CASE( implicitConversionToBase ) {
   xdm::RefPtr< Derived > ptr;
   takesBaseClass( ptr );
+}
+
+BOOST_AUTO_TEST_CASE( dynamicCast2 ) {
+  xdm::RefPtr< TestBase > basePtr( new TestDerived( 1, 2 ) );
+  xdm::RefPtr< TestDerived > derivedPtr = xdm::dynamic_pointer_cast< TestDerived >( basePtr );
+  BOOST_REQUIRE( derivedPtr.valid() );
+
+  // Check member access.
+  BOOST_CHECK_EQUAL( 1, derivedPtr->getIntBase() );
+  BOOST_CHECK_EQUAL( 2, derivedPtr->getInt() );
 }
 
 } // namespace
