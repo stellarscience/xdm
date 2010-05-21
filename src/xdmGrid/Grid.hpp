2d121b72cbfd28b731a45c5000b79e3b7c23b308
Todd Harrington <todd@stellarscience.com>
2010-05-21 09:49:56 -0600

    Code review matters.

--- src/xdmGrid/Grid.hpp
+++ src/xdmGrid/Grid.hpp
@@ -26,6 +26,9 @@
 
 #include <xdm/Forward.hpp>
 #include <xdm/Item.hpp>
+// Code Review Matter (open): Foward declaration
+// ItemVisitor does not seem to be used. Would a foraward declaration be sufficient?
+// -- Todd on 21 May 2010
 #include <xdm/ItemVisitor.hpp>
 #include <xdm/PrimitiveType.hpp>
 
@@ -33,6 +36,20 @@
 #include <vector>
 
 #include <xdmGrid/NamespaceMacro.hpp>
+
+// Code Review Matter (open): Optional namespace
+// It appears that namespaces are optional in the XDM code base; however, the code
+// uses them explicitly in many places.
+// Did you concider adding macros for when the name space is required rather than
+// only where it wraps code? For example:
+//
+// #define XDM_NS xdm::
+// class Grid : public XDM_NS Item { // I am not positive that would even work.
+//
+// If the library must support platforms that don't support namespaces, and since this could
+// possibley pollute the global namesapce with names that are likely to conflict (like Item),
+// did you consider using a library prefixes instead (XdmItem)?
+// -- Todd on 21 May 2010
 
 XDM_GRID_NAMESPACE_BEGIN
 
@@ -54,9 +71,26 @@ public:
 
   XDM_META_ITEM( Grid );
 
+  // Code Review Matter (open): Returning by value
+  // Did you consider returning by "const xdm::RefPtr< type >&" this would still result
+  // in a copy if the cleint code was not change, but may be a good standard practice
+  // to avoid unnesisary copies.
+  // -- Todd on 21 May 2010
+
+  // Code Review Matter (open): Access to private data
+  // Is there ever at time (pun intended) when it is appropriate for a client to
+  // have direct access to this member data?
+  // Did you consider only providing const access and the setter?
+  // -- Todd on 21 May 2010
+
   xdm::RefPtr< Time > time();
   xdm::RefPtr< const Time > time() const;
   void setTime( xdm::RefPtr< Time > time );
+
+  // Code Review Matter (open): Passing by value
+  // Did you consider passing by const "const xdm::RefPtr< type >&" this would eliminate
+  // one unnessiary copy opeartion.
+  // -- Todd on 21 May 2010
 
   /// Add an attribute definition to the grid.
   void addAttribute( xdm::RefPtr< Attribute > attribute );
